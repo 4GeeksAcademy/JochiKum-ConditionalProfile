13b7117db8404eed5d80c44b408209e360f6dee5
Jose Alejandro Moreno <166447988+JochiKum@users.noreply.github.com>
2024-07-25 01:14:19 +0000

    JochiKum-conditional-profile-card

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,28 @@
-import "../style/index.css";
-
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
  * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
+{
+  includeCover: true, // if includeCover is true the algorithm should show the cover image
+  background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
+  avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
+  socialMediaPosition: "right", // social media bar position (left or right)
+  
+  twitter: null, // social media usernames
+  github: null,
+  linkedin: null,
+  instagram: null,
+  
+  name: null,
+  lastName: null,
+  role: null,
+  country: null,
+  city: null
+  }
+  */
+import { left, right } from "@popperjs/core";
+import "../style/index.css";
 
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
   console.log("These are the current variables: ", variables); // print on the console
   // here we ask the logical questions to make decisions on how to build the html
@@ -30,17 +31,32 @@ function render(variables = {}) {
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
   // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+  document.querySelector("#widget_content").innerHTML = `
+  <div class="widget">
+  ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name ? variables.name : "Alejandra"} ${
+    variables.lastName ? variables.lastName : "Sanchez"
+  } </h1>
+          <h2>${variables.role ? variables.role : "FloorPlanner"}</h2>
+          <h3>${variables.city ? variables.city : "Caracas"} ${
+    variables.country ? variables.country : "Venezuela"
+  } </h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${
+              variables.twitter ? variables.twitter : "4GeeksAcademy"
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github ? variables.github : "4GeeksAcademy"
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href=${
+              variables.linkedin
+                ? `https://www.linkedin.com/in/${variables.linkedin}`
+                : "https://linkedin.com/school/4geeksacademy"
+            }><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram ? variables.instagram : "4GeeksAcademy"
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
